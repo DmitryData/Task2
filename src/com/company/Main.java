8901087c680b9e69ca2e1695b87ceea19564f95d
dmitryData <DeKar3D@hotmail.com>
2018-05-29 08:56:18 +0300

    fixing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,42 +7,45 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите два числа: " + "\n");
+        System.out.println("Введите два числа");
 
-        while(!in.hasNextLong()) {
+        while(!in.hasNextFloat()) {
             System.out.println("Ошибка ввода, надо вводить числа ");
             in.next();
         }
+        float value1 = in.nextFloat();
 
-        long value1 = in.nextLong();
-
-        while(!in.hasNextLong()) {
+        while(!in.hasNextFloat()) {
             System.out.println("Ошибка ввода, надо вводить числа ");
             in.next();
         }
-
-        long value2 = in.nextLong();
+        float value2 = in.nextFloat();
 
         summ(value1, value2);
         diff(value1, value2);
         lcm(value1, value2);
     }
 
-    private static void diff(long a, long b) {
-        long difference = a - a;
+    private static void diff(float a, float b) {
+        float difference;
+        if (a>b){
+            difference = a - b;
+        } else {
+            difference = b - a;
+        }
         System.out.println("Разность чисел равна: " + difference);
     }
 
-    private static void summ(long a, long b) {
-        long summ = a + b;
+    private static void summ(float a, float b) {
+        float summ = a + b;
         System.out.println("Сумма равна: " + summ);
     }
 
-    private static void lcm(long a,long b){
+    private static void lcm(float a,float b){
         System.out.println("НОК: " + a / gcd(a,b) * b);
     }
 
-    private static long gcd(long a,long b){
+    private static float gcd(float a,float b){
 
         if (b==0){
             System.out.println("НОД " + a);
@@ -51,6 +54,4 @@ public class Main {
             return gcd(b,a % b);
         }
     }
-
-
 }
